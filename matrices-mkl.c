a204ced2aa711ce473c42fb23dc832e6f043e69c
forbes1799 <123934016+forbes1799@users.noreply.github.com>
2025-02-17 11:00:20 +0000

    Add files via upload

--- matrices-mkl.c
+++ matrices-mkl.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <mkl_cblas.h>
 
-#define N 1009
+#define N 2048
 #define REPEATS 25
 
 // define a test element to be output
@@ -33,30 +33,19 @@ int main(void) {
     m2[i] = (float) rand()/(float) RAND_MAX;
     res[i] = 0.0;
   }
-  /* DEBUG
-  printf("m1\n");
-  for (i=0; i<N*N; i++) {
-    printf("%f ",m1[i]);
-  }
-  printf("\n");
-  printf("m2\n");
-  for (i=0; i<N*N; i++) {
-    printf("%f ",m2[i]);
-  }
-  printf("\n");
-  */
 
   // mat mat mult
   // repeat a number of times
   int repeat;
   for (repeat=0; repeat<REPEATS; repeat++) {
     printf("Run %d\n", repeat);
+    
     // reset res
     for (i=0; i<N*N; i++) {
       res[i] = 0.0;
     }
+
     timer[4] = omp_get_wtime();
-    //    mult(res, m1, m2);
 
     // mat mat mult LIBRARY
     // float => single precision -> sgemm
@@ -64,8 +53,10 @@ int main(void) {
     // sgemm (TRANSA, TRANSB, M, N, K, ALPHA, A, LDA, B, LDB, BETA, C, LDC)
     // to form C = alpha*A*B + beta*C
     cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, N,N,N, 1.0, m1, N, m2, N, 0.0, res,N);
+
     timer[5] = omp_get_wtime();
-    // output a val to avoid compiler removing
+
+    // output a val to avoid compiler removing res
     printf("res[outputElement] = %f\n",res[outputElement]); 
     printf("MAT MAT MULT: %f milliseconds\n\n", 1000.0*(timer[5]-timer[4]));
   }
--- matrices.c
+++ matrices.c
@@ -2,7 +2,7 @@
 #include <omp.h>
 #include <stdlib.h>
 
-#define N 1009
+#define N 2048
 #define REPEATS 25
 
 // define a test element to be output
@@ -27,52 +27,46 @@ int main(void) {
 
   // initialise
   for (i=0; i<N*N; i++) {
-    m1[i] = (float) rand()/(float) RAND_MAX;
-    m2[i] = (float) rand()/(float) RAND_MAX;
+	m1[i] = (float) rand()/(float) RAND_MAX;
+	m2[i] = (float) rand()/(float) RAND_MAX;
   }
-    /*  DEBUG
-  printf("m1\n");
-  for (i=0; i<N*N; i++) {
-    printf("%f ",m1[i]);
-  }
-  printf("\n");
-  printf("m2\n");
-  for (i=0; i<N*N; i++) {
-    printf("%f ",m2[i]);
-  }
-  printf("\n");
-    */
+ 
   // mat mat mult
   // repeat a number of times
   int repeat;
   for (repeat=0; repeat<REPEATS; repeat++) {
-    printf("Run %d\n", repeat);
-    // set/reset res
-    for (i=0; i<N*N; i++) {
-      res[i] = 0.0;
-    }
-    timer[4] = omp_get_wtime();
-    mult(res, m1, m2);
-    timer[5] = omp_get_wtime();
-    // output a val to avoid compiler removing
-    printf("res[outputElement] = %f\n",res[outputElement]); 
-    printf("MAT MAT MULT: %f milliseconds\n\n", 1000.0*(timer[5] - timer[4]));
+	printf("Run %d\n", repeat);
+	
+	// set/reset res
+	for (i=0; i<N*N; i++) {
+	  res[i] = 0.0;
+	}
+
+	timer[4] = omp_get_wtime();
+	mult(res, m1, m2);
+	timer[5] = omp_get_wtime();
+
+	// output a val to avoid compiler removing
+	printf("res[outputElement] = %f\n",res[outputElement]); 
+	printf("MAT MAT MULT: %f milliseconds\n\n", 1000.0*(timer[5] - timer[4]));
   }
 }
 
 void mult(float *res, float *m1, float *m2) {
-  int i,j,k;
-    for (i=0; i<N; i++) {
-      for (j=0; j<N; j++) { 
-	      for (k=0; k<N; k++) {
-	        int ij = i*N+j;
-	        int kj = k*N+j;
-	        int ik = i*N+k;
-	   	    //res[i][j] += m1[i][k] * m2[k][j];
-	        //printf("res (%d) will be increased from %f by %f * %f to...", ij, res[ij], m1[ik], m2[kj]);
-	        res[ij] += m1[ik] * m2[kj];
-	        //printf("  %f\n", res[ij]);
-	    }
-      }
-    }
+	int i,j,k;
+
+	for (i=0; i<N; i++) {
+		for (j=0; j<N; j++) { 
+			for (k=0; k<N; k++) {
+
+				int ij = i*N+j;
+				int ik = i*N+k;
+				int kj = k*N+j;
+
+				//res[i][j] += m1[i][k] * m2[k][j];
+				res[ij] += m1[ik] * m2[kj];
+				
+			}
+		}
+	}
 }
