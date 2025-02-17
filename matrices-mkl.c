5f4ef094fb013be456eb3a69a8b51c69684d94a6
forbes1799 <123934016+forbes1799@users.noreply.github.com>
2025-02-17 11:04:44 +0000

    Add files via upload

--- matrices-mkl.c
+++ matrices-mkl.c
@@ -5,76 +5,57 @@
 #include <mkl_cblas.h>
 
 #define N 2048
-#define REPEATS 25
+#define REPEATS 5
 
 // define a test element to be output
 #define NOUT 1
 #define outputElement NOUT*N+NOUT
 
-void mult(float*, float*, float*);
-
 int main(void) {
-  float *m1, *m2, *res;
-  int   matrixByte = N*N*sizeof(float);
-  int i,j,k;
-  double timer[8];
+	float *m1, *m2, *res;
+	int   matrixByte = N*N*sizeof(float);
+	int i,j,k;
+	double timer[8];
 
-  printf("\n\n R U N N I N G ... ... \n\n");
-  printf("N=%d\n", N);
+	printf("\n\n R U N N I N G ... ... \n\n");
+	printf("N=%d\n", N);
 
-  // malloc everybody
-  m1 = malloc(matrixByte);
-  m2 = malloc(matrixByte);
-  res = malloc(matrixByte);
+	// malloc everybody
+	m1 = malloc(matrixByte);
+	m2 = malloc(matrixByte);
+	res = malloc(matrixByte);
 
-  // initialise
-  for (i=0; i<N*N; i++) {
-    m1[i] = (float) rand()/(float) RAND_MAX;
-    m2[i] = (float) rand()/(float) RAND_MAX;
-    res[i] = 0.0;
-  }
-
-  // mat mat mult
-  // repeat a number of times
-  int repeat;
-  for (repeat=0; repeat<REPEATS; repeat++) {
-    printf("Run %d\n", repeat);
-    
-    // reset res
-    for (i=0; i<N*N; i++) {
-      res[i] = 0.0;
-    }
-
-    timer[4] = omp_get_wtime();
-
-    // mat mat mult LIBRARY
-    // float => single precision -> sgemm
-    // sgemm is very generalised call sgemm. from `man sgemm`
-    // sgemm (TRANSA, TRANSB, M, N, K, ALPHA, A, LDA, B, LDB, BETA, C, LDC)
-    // to form C = alpha*A*B + beta*C
-    cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, N,N,N, 1.0, m1, N, m2, N, 0.0, res,N);
-
-    timer[5] = omp_get_wtime();
-
-    // output a val to avoid compiler removing res
-    printf("res[outputElement] = %f\n",res[outputElement]); 
-    printf("MAT MAT MULT: %f milliseconds\n\n", 1000.0*(timer[5]-timer[4]));
-  }
-}
-
-void mult(float *res, float *m1, float *m2) {
-  int i,j,k;
-    for (i=0; i<N; i++) {
-      for (j=0; j<N; j++) { 
-	for (k=0; k<N; k++) {
-	  int ij = i*N+j;
-	  int kj = k*N+j;
-	  int ik = i*N+k;
-	  //	  res[i][j] += m1[i][k] * m2[k][j];
-	  //DBG printf("res (%d) will be increased from %f by %f * %f to...", ij, res[ij], m1[ik], m2[kj]);
-	  res[ij] += m1[ik] * m2[kj];
-	  //DBG printf("  %f\n", res[ij]);
+	// initialise
+	for (i=0; i<N*N; i++) {
+		m1[i] = (float) rand()/(float) RAND_MAX;
+		m2[i] = (float) rand()/(float) RAND_MAX;
+		res[i] = 0.0;
 	}
-      }
-    }
+
+	// mat mat mult
+	// repeat a number of times
+	int repeat;
+	for (repeat=0; repeat<REPEATS; repeat++) {
+		printf("Run %d\n", repeat+1);
+		
+		// reset res
+		for (i=0; i<N*N; i++) {
+			res[i] = 0.0;
+		}
+
+		timer[4] = omp_get_wtime();
+
+		// mat mat mult LIBRARY
+		// float => single precision -> sgemm
+		// sgemm is very generalised call sgemm. from `man sgemm`
+		// sgemm (TRANSA, TRANSB, M, N, K, ALPHA, A, LDA, B, LDB, BETA, C, LDC)
+		// to form C = alpha*A*B + beta*C
+		cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, N,N,N, 1.0, m1, N, m2, N, 0.0, res,N);
+
+		timer[5] = omp_get_wtime();
+
+		// output a val to avoid compiler removing res
+		printf("res[outputElement] = %f\n",res[outputElement]); 
+		printf("MAT MAT MULT: %f milliseconds\n\n", 1000.0*(timer[5]-timer[4]));
+	}
 }
--- matrices.c
+++ matrices.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 #define N 2048
-#define REPEATS 25
+#define REPEATS 5
 
 // define a test element to be output
 #define NOUT 1
@@ -12,44 +12,44 @@
 void mult(float*, float*, float*);
 
 int main(void) {
-  float *m1, *m2, *res;
-  int   matrixByte = N*N*sizeof(float);
-  int i,j,k;
-  double timer[8];
+	float *m1, *m2, *res;
+  	int   matrixByte = N*N*sizeof(float);
+	int i,j,k;
+	double timer[8];
 
-  printf("\n\n R U N N I N G ... ... \n\n");
-  printf("N=%d\n", N);
+	printf("\n\n R U N N I N G ... ... \n\n");
+	printf("N=%d\n", N);
 
-  // malloc everybody
-  m1 = (float*)malloc(matrixByte);
-  m2 = (float*)malloc(matrixByte);
-  res = (float*)malloc(matrixByte);
+	// malloc everybody
+	m1 = (float*)malloc(matrixByte);
+	m2 = (float*)malloc(matrixByte);
+	res = (float*)malloc(matrixByte);
 
-  // initialise
-  for (i=0; i<N*N; i++) {
-	m1[i] = (float) rand()/(float) RAND_MAX;
-	m2[i] = (float) rand()/(float) RAND_MAX;
-  }
- 
-  // mat mat mult
-  // repeat a number of times
-  int repeat;
-  for (repeat=0; repeat<REPEATS; repeat++) {
-	printf("Run %d\n", repeat);
-	
-	// set/reset res
+	// initialise
 	for (i=0; i<N*N; i++) {
-	  res[i] = 0.0;
+		m1[i] = (float) rand()/(float) RAND_MAX;
+		m2[i] = (float) rand()/(float) RAND_MAX;
 	}
+ 
+	// mat mat mult
+	// repeat a number of times
+	int repeat;
+	for (repeat=0; repeat<REPEATS; repeat++) {
+		printf("Run %d\n", repeat+1);
+		
+		// set/reset res
+		for (i=0; i<N*N; i++) {
+			res[i] = 0.0;
+		}
 
-	timer[4] = omp_get_wtime();
-	mult(res, m1, m2);
-	timer[5] = omp_get_wtime();
+		timer[4] = omp_get_wtime();
+		mult(res, m1, m2);
+		timer[5] = omp_get_wtime();
 
-	// output a val to avoid compiler removing
-	printf("res[outputElement] = %f\n",res[outputElement]); 
-	printf("MAT MAT MULT: %f milliseconds\n\n", 1000.0*(timer[5] - timer[4]));
-  }
+		// output a val to avoid compiler removing
+		printf("res[outputElement] = %f\n",res[outputElement]); 
+		printf("MAT MAT MULT: %f milliseconds\n\n", 1000.0*(timer[5] - timer[4]));
+	}
 }
 
 void mult(float *res, float *m1, float *m2) {
